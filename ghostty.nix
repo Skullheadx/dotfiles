0b8810de255b3a6155272125ec611b47ec44a022
Andrew <admonty1@protonmail.com>
2025-08-22 23:59:41 -0400

    ghostty configuration

--- ghostty.nix
+++ ghostty.nix
@@ -8,9 +8,16 @@
   programs.ghostty = {
     enable = true;
     settings = {
+      shell-integration = "zsh";
+      shell-integration-features = true;
+      mouse-hide-while-typing = true;
       link-url = true;
       background-opacity = 0.8;
       background-blur = true;
+      clipboard-read = "allow";
+      clipboard-write = "allow";
+      confirm-close-surface = false;
+      auto-update = "off";
 
     };
   };
