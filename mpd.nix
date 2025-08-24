9b3e72107cb25cadb22f4eb9d13b9004dfcb6cde
Andrew <admonty1@protonmail.com>
2025-08-24 03:04:42 -0400

    todo for mpd

--- mpd.nix
+++ mpd.nix
@@ -4,6 +4,7 @@
     enable = true;
     musicDirectory = "${config.home.homeDirectory}/Music";
     playlistDirectory = "${config.home.homeDirectory}/.playlists";
+    # mixer_type "none" # maybe mess around with this some time, it will turn off volume in rmpc, but apparently it will make the sound more quality for music
     extraConfig = ''
       		      auto_update "yes"
 		      audio_output {
