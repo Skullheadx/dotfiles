a4b96458d1454f8c854bbeb0023de425e1503d23
Andrew <admonty1@protonmail.com>
2025-10-28 23:41:45 -0400

    update ghostty

--- ghostty.nix
+++ ghostty.nix
@@ -3,22 +3,28 @@
   pkgs,
   inputs,
   ...
-}:
-{
+}: {
   programs.ghostty = {
     enable = true;
     settings = {
-      shell-integration = "zsh";
+      shell-integration = "fish";
       shell-integration-features = true;
       mouse-hide-while-typing = true;
+      cursor-click-to-move = true;
+      mouse-shift-capture = true;
+      scrollback-limit = 10000;
       link-url = true;
+      link-previews = true;
       background-opacity = 0.8;
       background-blur = true;
+      window-inherit-working-directory = true;
+      window-save-state = "always";
+      copy-on-select = "clipboard";
       clipboard-read = "allow";
+
       clipboard-write = "allow";
       confirm-close-surface = false;
       auto-update = "off";
-
     };
   };
 }
