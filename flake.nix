8617488dc280d3fde957d4ea09f18fd7c6b14628
Skullheadx <admonty1@protonmail.com>
2026-01-28 16:39:14 -0500

    extraspecial args

--- flake.nix
+++ flake.nix
@@ -61,6 +61,7 @@
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
             # home-manager.extraSpec ialArgs = [ inputs ];
+            home-manager.extraSpecialArgs = {customNeovim = customNeovim;};
           })
         ];
       };
