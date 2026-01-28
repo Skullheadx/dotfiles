30d175175b067d53f68320d8c1c4c9a148799966
Skullheadx <admonty1@gmail.com>
2026-01-28 03:54:34 -0500

    zsh

--- sh.nix
+++ sh.nix
@@ -1,10 +1,42 @@
-{ config
-, lib
-, pkgs
-, ...
+{
+  config,
+  lib,
+  pkgs,
+  ...
 }: {
   programs.bash = {
     enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true; # Enables zsh-completions
+    autosuggestion.enable = true; # Enables zsh-autosuggestions
+    syntaxHighlighting.enable = true; # Enables zsh-syntax-highlighting
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git" # Git plugin
+        "colored-man-pages" # Colored man pages
+        "alias-finder" # Find aliases for commands
+      ];
+      theme = "";
+    };
+    initContent = ''
+       # Powerlevel10k configuration
+       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+        source ${./.p10k.zsh}
+       # Initialize zoxide
+       eval "$(zoxide init zsh)"
+
+       # Initialize fzf
+       source <(fzf --zsh)
+
+       # Source fzf-tab (needs to be after fzf)
+       source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+
+      bindkey '^ ' autosuggest-execute
+    '';
   };
   home.packages = with pkgs; [
     zoxide
