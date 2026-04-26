f37bded02091fb9ccab76c287b70cdb0c2c8e5a4
Skullheadx <admonty1@protonmail.com>
2026-04-25 22:44:05 -0400

    add mpd

--- configuration.nix
+++ configuration.nix
@@ -146,6 +146,21 @@
     };
   };
 
+#  services.mpd = {
+#    enable = true;
+#    musicDirectory = "${config.home.homeDirectory}/Music";
+#    playlistDirectory = "${config.home.homeDirectory}/.playlists";
+#    # mixer_type "none" # maybe mess around with this some time, it will turn off volume in rmpc, but apparently it will make the sound more quality for music
+#     extraConfig = ''
+#       		      auto_update "yes"
+# 		      audio_output {
+# 			type "pulse"
+# 			name "PipeWire Output"
+# 		      }
+#       	      	'';
+#  };
+
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
