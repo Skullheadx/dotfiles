2101c58ce12ceed89352b195740e259460824bad
Skullheadx <admonty1@protonmail.com>
2026-04-26 00:50:08 -0400

    move sxhkdrc to dotfiles and replaceVars

--- hjem.nix
+++ hjem.nix
@@ -18,35 +18,16 @@
     directory = "/home/andrew";
     files = {
 
-      ".config/sxhkd/sxhkdrc".text = ''
-super + space
-	${pkgs.dmenu}/bin/dmenu_run
-
-super + Return
-	${pkgs.st}/bin/st	
-
-super + b
-	${pkgs.librewolf}/bin/librewolf
-
-XF86AudioRaiseVolume
-    ${pkgs.pamixer}/bin/pamixer -i 5
-
-XF86AudioLowerVolume
-    ${pkgs.pamixer}/bin/pamixer -d 5
-
-XF86AudioMute
-    ${pkgs.pamixer}/bin/pamixer -t
-
-super + s
-    ${pkgs.maim}/bin/maim -i $(${pkgs.xdotool}/bin/xdotool getactivewindow) | ${pkgs.xclip}/bin/xclip -selection clipboard -t image/png
-
-super + shift + s
-	${pkgs.maim}/bin/maim -s | ${pkgs.xclip}/bin/xclip -selection clipboard -t image/png
-
-super + l
-	${pkgs.lock-screen}/bin/lock-screen 
-
-      '';
+      ".config/sxhkd/sxhkdrc".text = builtins.readFile (pkgs.replaceVars ./dotfiles/sxhkdrc {
+		dmenu = pkgs.dmenu;
+	st = pkgs.st;
+      librewolf = pkgs.librewolf;
+      pamixer = pkgs.pamixer;
+      maim = pkgs.maim;
+      xdotool = pkgs.xdotool;
+      xclip = pkgs.xclip;
+      lockscreen = pkgs.lock-screen;
+});
     };
     packages = with pkgs; [
       discord
