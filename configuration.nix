53e34d323b360c3a69d2647d0a6bfefe70265c01
Skullheadx <admonty1@protonmail.com>
2026-05-01 12:12:30 -0400

    bash and readline config

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,43 @@
     };
   };
 
+  programs.bash = {
+    enable = true;
+    interactiveShellInit = ''
+      shopt -s autocd
+      shopt -s cdable_vars
+      shopt -s cdspell
+      shopt -s dirspell
+      shopt -s checkjobs
+      shopt -s cmdhist
+      shopt -s histappend
+      shopt -s globstar
+      shopt -s extglob
+    '';
+    promptInit = ''
+      PS1="\[\e[97m\][\[\e[m\]\[\e[92m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[92m\]\h\[\e[m\]:\[\e[92m\]\w\[\e[m\]\[\e[97m\]]\[\e[m\]\[\e[97m\]\\$\[\e[m\] "
+    '';
+  };
+
+  environment.etc."inputrc".text = ''
+    set editing-mode vi
+    set show-mode-in-prompt on
+    set keyseq-timeout 10
+
+    set vi-ins-mode-string "\1\e[5 q\2"
+    set vi-cmd-mode-string "\1\e[2 q\2"
+
+
+    set colored-stats on
+    set colored-completion-prefix on
+    set blink-matching-paren on
+
+    set completion-ignore-case on
+    set show-all-if-ambiguous on
+    set completion-map-case on
+  '';
+
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
