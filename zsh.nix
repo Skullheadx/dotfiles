54657feb40b32fd8765636cac1285a8cae7670ea
Skullheadx <andrew.montgomery@warpengine.ai>
2026-06-29 12:10:41 -0400

    parity with bash for darwin zsh

--- zsh.nix
+++ zsh.nix
@@ -8,12 +8,21 @@
     enableAutosuggestions = true;
     enableBashCompletion = true;
     enableCompletion = true;
+    enableGlobalCompInit = true;
     enableFastSyntaxHighlighting = true;
     enableFzfCompletion = true;
     enableFzfGit = true;
     enableFzfHistory = true;
+    histFile = "$HOME/.zsh_history";
+    histSize = 100000;
+    promptInit = ''
+      PROMPT=$'%{\e[97m%}[%{\e[m%}%{\e[92m%}%n%{\e[m%}%{\e[32m%}@%{\e[m%}%{\e[92m%}%m%{\e[m%}:%{\e[92m%}%~%{\e[m%}%{\e[97m%}]%{\e[m%}%{\e[97m%}%#%{\e[m%} '
+    '';
     interactiveShellInit = ''
       eval "$(direnv hook zsh)"
+
+      bindkey -v
+      export KEYTIMEOUT=1
     '';
   };
 }
