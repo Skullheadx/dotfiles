98103d38c525791228da1e3fcbb1021e6c02cfce
Skullheadx <andrew@andrew-montgomery.dev>
2026-06-30 12:43:03 -0400

    try git delta

--- hjem-darwin.nix
+++ hjem-darwin.nix
@@ -11,9 +11,11 @@
     directory = "/Users/${username}";
     files = {
       ".config/ghostty/config.ghostty".source = ./dotfiles/ghostty/config.ghostty;
+      ".config/git/config".source = ./dotfiles/git/config;
     };
 
     packages = with pkgs; [
+      delta
     ];
   };
 }
