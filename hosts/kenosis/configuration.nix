f2549e471052f5aa146280f9bca8bdc8a5097e9a
Skullheadx <andrew.montgomery@warpengine.ai>
2026-06-29 20:57:28 -0400

    kenosis known hosts and git proxy jump

--- hosts/kenosis/configuration.nix
+++ hosts/kenosis/configuration.nix
@@ -18,6 +18,40 @@
   };
   # Primary user for user-specific settings (dock, etc.)
   system.primaryUser = username;
+
+  programs.ssh = {
+    knownHosts = {
+      homelab = {
+        extraHostNames = ["192.168.1.120"];
+        publicKeyFile = ./../../pubkeys/homelab_ssh.pub;
+      };
+      vps = {
+        extraHostNames = ["170.205.37.7"];
+        publicKeyFile = ./../../pubkeys/vps_ssh.pub;
+      };
+      github = {
+        extraHostNames = ["github.com"];
+        publicKeyFile = ./../../pubkeys/github_ssh.pub;
+      };
+    };
+    extraConfig = ''
+      Host git-vps
+        HostName git.skullheadx.com
+        Port 2222
+        User git
+      Host git.skullheadx.com
+        HostName localhost
+        Port 2223
+        User git
+        ProxyJump git-vps
+      Host homelab
+        HostName 192.168.1.120
+        Port 22
+      Host vps
+        Hostname 170.205.37.7
+        Port 2222
+    '';
+  };
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
