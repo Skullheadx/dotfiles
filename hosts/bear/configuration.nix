b3f3c0956aaf63364a715d65d739244761f5fad7
Skullheadx <admonty1@gmail.com>
2026-08-13 00:45:09 -0400

    remove laptop git vps ssh config

--- hosts/bear/configuration.nix
+++ hosts/bear/configuration.nix
@@ -49,15 +49,6 @@
       };
     };
     extraConfig = ''
-      Host git-vps
-        HostName git.skullheadx.com
-        Port 2222
-        User git
-      Host git.skullheadx.com
-        HostName localhost
-        Port 2223
-        User git
-        ProxyJump git-vps
       Host homelab
         HostName 192.168.1.120
         Port 22
