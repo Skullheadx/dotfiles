d737c5d5394064734e05fc21bb3d91b9d092cff5
Skullheadx <admonty1@gmail.com>
2026-08-16 22:42:39 -0400

    turn off ssh on bear

--- hosts/bear/configuration.nix
+++ hosts/bear/configuration.nix
@@ -12,25 +12,25 @@
   ];
 
   # IMPORTANT Update this in all other hosts if changed
-  services.openssh = {
-    enable = true;
-    enableRecommendedAlgorithms = true;
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
+  # services.openssh = {
+  #   enable = true;
+  #   settings = {
+  #     PasswordAuthentication = false;
+  #     KbdInteractiveAuthentication = false;
+  #
+  #     PermitRootLogin = "no";
+  #
+  #     PubkeyAuthentication = "yes";
+  #     # MaxAuthTries = 3;
+  #     # LoginGraceTime = "30s";
+  #
+  #     # X11Forwarding = false;
+  #     # AllowAgentForwarding = false;
+  #     # AllowTcpForwarding = true;
+  #   };
+  #   ports = [2222];
+  # };
 
-      PermitRootLogin = "no";
-
-      PubkeyAuthentication = "yes";
-      # MaxAuthTries = 3;
-      # LoginGraceTime = "30s";
-
-      # X11Forwarding = false;
-      # AllowAgentForwarding = false;
-      # AllowTcpForwarding = true;
-    };
-    ports = [2222];
-  };
   networking = {
     computerName = "bear";
     hostName = "bear";
