6a3be9b46807f561482095ca223cc7b077a28370
Skullheadx <andrew@montgomery.systems>
2026-08-17 22:56:50 -0400

    break mpd

--- hosts/nepsis/audio-linux.nix
+++ hosts/nepsis/audio-linux.nix
@@ -4,15 +4,17 @@
   username,
   ...
 }: {
-  # TODO: Why is pipewire AND wireplumber enabled?
+  # Whether to enable the RealtimeKit system service, which hands out realtime scheduling priority to user processes on demand. For example, PulseAudio and PipeWire use this to acquire realtime priority.
+  security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
     audio.enable = true;
     pulse.enable = true;
     alsa.enable = true;
+    wireplumber.enable = true;
   };
-
-  services.pipewire.wireplumber.enable = true;
+  services.pulseaudio.enable = false;
 
   # TODO: WTF is this?
   hardware = {
@@ -21,21 +23,20 @@
     };
   };
 
-  # TODO: Why am I using this instead of services.mpd.enable = true
   services.mpd = {
     enable = true;
+    user = username;
     settings = {
-      music_directory = "/home/${username}";
-      playlist_directory = "/home/${username}/playlists";
+      music_directory = "/home/${username}/Music";
+      playlist_directory = "/home/${username}/Music/playlists";
       # Old Config
       # music_directory    "~/Music"
       # playlist_directory "~/Music/playlists"
       # db_file            "~/.local/share/mpd/database"
       # state_file         "~/.local/share/mpd/state"
       # log_file           "syslog"
-      #
-      # auto_update "yes"
-      # restore_paused "yes"
+      # auto_update = true;
+      # restore_paused = true;
 
       audio_output = [
         {
@@ -44,5 +45,10 @@
         }
       ];
     };
+  };
+  systemd.services.mpd = {
+    environment.XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.andrew.uid}";
+    serviceConfig.Restart = "on-failure";
+    serviceConfig.RestartSec = 2;
   };
 }
